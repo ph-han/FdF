c68c965c2724b21674f724089ca5c88071121b8a
phan <hanfeelhoo@naver.com>
2024-01-20 19:42:38 +0900

    [refactor] : 폴더/파일 정리

--- includes/fdf.h
+++ includes/fdf.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_BONUS_H
-# define FDF_BONUS_H
+#ifndef FDF_H
+# define FDF_H
 
 # include <mlx.h>
 # include <unistd.h>
